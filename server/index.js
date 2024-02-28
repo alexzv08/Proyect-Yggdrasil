93a11da25607240fbce780daf96a7ae5277495e2
alexzv08 <alexzv8@gmail.com>
2024-02-28 18:03:11 +0100

    cambios server

--- server/index.js
+++ server/index.js
@@ -21,22 +21,22 @@ let resultsDatabase;
  * @tutorial 
  * CONEXION A LA BASE DE DATOS LOCAL
  */
-const connection = await mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'admini',
-    database: 'prueba',
-    authPlugins: ['mysql_native_password'] // Add this line
-});
+// const connection = await mysql.createConnection({
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: 'admini',
+//     database: 'prueba',
+//     authPlugins: ['mysql_native_password'] // Add this line
+// });
 
 /**
  * @tutorial 
  * CONEXION A LA BASE DE DATOS EN LA NUVE **RAILWAY**
  */
-// const connection = await mysql.createConnection(
-//     process.env.DATABASE_URL
-// );
+const connection = await mysql.createConnection(
+    process.env.DATABASE_URL
+);
 
 connection.connect((error) => {
   if (error) {
