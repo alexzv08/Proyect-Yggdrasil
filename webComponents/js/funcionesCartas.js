9bb1e374c1427b0462c08faa3f0fe52146b7e52f
alexzv08 <alexzv8@gmail.com>
2024-03-18 06:59:10 +0100

    cosass

---
+++ webComponents/js/funcionesCartas.js
@@ -0,0 +1,22 @@
+function imgCartas(){
+    fetch("https://digimoncard.io/api-public/search.php?sort=code&series=Digimon Card Game")
+    .then(data => data.json())
+    .then(data =>{
+        console.log(data)
+        data.forEach(element => {
+            cargarImg(element.image_url)
+        });
+        // imgCard.src = data[0].image_url
+    })  
+    console.log('ho');
+}
+function cargarImg(element){
+    let div = document.createElement("div")
+    let img = document.createElement("img")
+    img.src = element
+    containerCartas.appendChild(div)
+    div.appendChild(img)
+}
+export const methods = {
+    imgCartas, cargarImg
+}
--- webComponents/js/sideBar.js
+++ webComponents/js/sideBar.js
@@ -1,11 +1,26 @@
-let ocultoMenu=false;
+import { methods as funcionesCartas } from "./funcionesCartas.js";
+let ocultoMenu, ocultoChatHamburger=false;
 let ocultoChat=false;
 
 window.onload = ()=>{
     toogleMenu.addEventListener("click", toggleMenuChange)
-    toogleChat.addEventListener("click", toggleChatChange)
+    hamnurgerMenu.addEventListener("click", toggleMenuChangeHamburger)
 
+    // toogleChat.addEventListener("click", toggleChatChange)
+    // deckbuilder.addEventListener("click", insertCartas)
 }
+
+async function insertCartas(){
+    contenidoWeb.innerHTML =""
+    await fetch("seccionCartas.html")
+    .then(data => data.text())
+    .then(data =>{
+        contenidoWeb.insertAdjacentHTML("beforeend", data)
+    })
+    funcionesCartas.imgCartas()
+    console.log("hola")
+}
+
 
 function toggleMenuChange(){
     let titulos = document.querySelectorAll("span h3")
@@ -15,33 +30,53 @@ function toggleMenuChange(){
             element.style.display = "block"
         });
         toogleMenu.style.left="255px";
-        containerSidebar.style.width = "270px";
-        console.log(containerSidebar.offsetWidth);
-        console.log(document.querySelector("body").offsetWidth);
-        contenidoWeb.style.width=`calc(100% - 270px)`
+        containerSidebar.classList.remove("menuOculto")
+        containerSidebar.classList.add("menuDesplegado")
+        // console.log(containerSidebar.offsetWidth);
+        // console.log(document.querySelector("body").offsetWidth);
+        // contenidoWeb.style.width=`calc(100% - 270px)`
 
         ocultoMenu=false
     }else{
         titulos.forEach(element => {
             element.style.display = "none"
         });
-        containerSidebar.style.width = "100px";
+        containerSidebar.classList.add("menuOculto")
+        containerSidebar.classList.remove("menuDesplegado")
         toogleMenu.style.left="85px";
-        console.log(containerSidebar.offsetWidth);
-        console.log(document.querySelector("body").offsetWidth);
+        // console.log(containerSidebar.offsetWidth);
+        // console.log(document.querySelector("body").offsetWidth);
 
-        contenidoWeb.style.width=`calc(100% - 100px)`
+        // contenidoWeb.style.width=`calc(100% - 100px)`
         ocultoMenu=true
     }
 }
-function toggleChatChange(){
-    if(ocultoChat){
-        chatContainer.style.height="100%";
-        chat.style.display="block";
-        ocultoChat=false
+function toggleMenuChangeHamburger(){
+    let hamburger = document.querySelectorAll("#hamnurgerMenu div div")
+
+    if(!ocultoChatHamburger){
+        hamburger[0].classList.add('x1')
+        hamburger[1].classList.add('x2')
+        hamburger[0].style.margin="0"
+        hamburger[1].style.margin="0"
+
+        ocultoChatHamburger=true
     }else{
-        chatContainer.style.height="50px";
-        chat.style.display="none";
-        ocultoChat=true
+        hamburger[0].classList.remove('x1')
+        hamburger[1].classList.remove('x2')
+        hamburger[0].style.margin="10px"
+        hamburger[1].style.margin="10px"
+        ocultoChatHamburger=false
     }
 }
+// function toggleChatChange(){
+//     if(!ocultoChat){
+//         chatContainer.style.height="100%";
+//         chat.style.display="block";
+//         ocultoChat=true
+//     }else{
+//         chatContainer.style.height="50px";
+//         chat.style.display="none";
+//         ocultoChat=false
+//     }
+// }
