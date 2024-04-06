360ecd6b8539fde2157c94290114c8ce9f3e87f6
alexzv08 <alexzv8@gmail.com>
2024-04-06 10:03:19 +0200

    a

--- server/controllers/autentificador.js
+++ server/controllers/autentificador.js
@@ -12,22 +12,6 @@ import bcryptjs from 'bcryptjs';
 const connection = await mysql.createConnection(
     process.env.DATABASE_URL
 );
-// const connection = await mysql.createConnection({
-//     host: 'localhost',
-//     port: 3306,
-//     user: 'root',
-//     password: '',
-//     database: 'prueba',
-//     authPlugins: ['mysql_native_password'] // Add this line
-// });
-
-/**
- * @tutorial 
- * CONEXION A LA BASE DE DATOS EN LA NUVE **RAILWAY**
- */
-const connection = await mysql.createConnection(
-    process.env.DATABASE_URL
-);
 
 connection.connect((error) => {
     if (error) {
